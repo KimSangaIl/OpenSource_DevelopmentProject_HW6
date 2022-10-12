acc749f6b5081ea29786238a5d831b4f784f1434
KimSangil <48938699+KimSangaIl@users.noreply.github.com>
2022-10-12 10:35:48 +0900

    Add files via upload
    ex 6-6

---
+++ AutoBoxingUnBoxingEx.java
@@ -0,0 +1,13 @@
+/*
+ * 소스 파일 : AutoBoxingUnBoxingEx.java
+ */
+public class AutoBoxingUnBoxingEx {
+	public static void main(String [] args) {
+		int n = 10;
+		Integer intObject = n;
+		System.out.println("intObject = " + intObject);
+		
+		int m = intObject + 10;
+		System.out.println("m = " + m);
+	}
+}
