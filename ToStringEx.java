5daa1f58f21cc4fbf2393cbab9434b9071ec9cb1
KimSangil <48938699+KimSangaIl@users.noreply.github.com>
2022-10-12 10:34:14 +0900

    Add files via upload
    ex 6-2

---
+++ ToStringEx.java
@@ -0,0 +1,22 @@
+/*
+ * 소스 파일 : ToStringEx.java
+ */
+class Point2 {
+	private int x, y;
+	public Point2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public String toString() {
+		return "Point2(" + x + "," + y + ")";
+	}
+}
+
+public class ToStringEx {
+	public static void main(String [] args) {
+		Point2 p = new Point2(2,3);
+		System.out.println(p.toString());
+		System.out.println(p);
+		System.out.println(p + "입니다.");
+	}
+}
